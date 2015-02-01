c2a038a1c08dab90a6f4cd712f5660d3e93a046f
Ash Robinson <Robinson@Ashleys-MacBook-Pro.local>
2015-02-01 18:28:26 +0000

    Initial commit from source control.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var historyLabel: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false;
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        var digit = sender.currentTitle!
+        
+        if digit == "." && display.text!.rangeOfString(".") != nil {
+            return;
+        }
+        
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        
+        appendToHistoryLabel(digit)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton!) {
+        var operation = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        switch operation {
+        case "x": performOperation { $0 * $1 }
+        case "-": performOperation { $1 - $0 }
+        case "/": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "√": performOperation { sqrt($0) }
+        case "SIN": performOperation { sin($0) }
+        case "COS": performOperation { cos($0) }
+        case "π": performOperation { $0 * M_PI }
+        default: break
+        }
+        
+        appendToHistoryLabel(" " + operation + " ")
     }
-
-
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func appendToHistoryLabel(text: String) {
+        historyLabel.text = historyLabel.text! + text;
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        operandStack.removeAll(keepCapacity: false)
+        display.text = ""
+        historyLabel.text = ""
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
 }
 
